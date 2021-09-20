c14a8b65081743fc6e8961b844058b840ac13c3a
Ivan Giokov <ivan.giokov@gmail.com>
2021-09-20 15:36:17 +0300

    Clean up, Setting: Basic structure, Redux and Router

---
+++ src/utility/NavBar/NavBar.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import './NavBar.css'
+
+class NavBar extends Component{
+    render(){
+        return(
+            <h1>Navbar</h1>
+        )
+    }
+}
+
+export default NavBar
